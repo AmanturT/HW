84b180f3b196d6c8908393446a2117c972bcd269
AmanturT <davleszutanov@gmail.com>
2024-02-27 19:42:22 +0300

    Third Commit

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -4,6 +4,14 @@
 #include "Helpers.h"
 int main()
 {
-    std::cout << result(2,5);
+    //std::cout << result(2,5) Прошлое ДЗ(13)
+    
+
+    //Нынешнее ДЗ(14)
+    /*std::string str = "Hello World";
+    std::cout << str << std::endl;
+    std::cout << str.length() << std::endl;
+    std::cout << str.at(0) << std::endl;
+    std::cout << str.at(str.length() - 1) << std::endl;*/
 }
 
